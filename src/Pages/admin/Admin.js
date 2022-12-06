cb4c25338ada4b5061694f3cdd8bf6e4a9702f88
manoj <manojravula929@gmail.com>
2022-12-06 22:48:45 +0530

    movie table

--- src/Pages/admin/Admin.js
+++ src/Pages/admin/Admin.js
@@ -25,47 +25,86 @@ const Admin = () => {
     //update the counterInfo state
     const datafromAPI = [
       {
+        sr_no: '01',
         name: "PVR",
         city: "Guntur",
         description: "Multi screen cinema",
-        pinCode: 522007,
+        screen:5,
+        pincode: 522007
       },
       {
+        sr_no: '02',
         name: "UV Platinos 4K",
         city: "Guntur",
         description: "Gold cinema",
-        pinCode: 522007,
+        screen:5,
+        pincode: 522007
       },
       {
+        sr_no: '03',
         name: "Cine square",
         city: "Guntur",
+        screen:1,
         description: "70mm Dolby Atmos",
-        pinCode: 522007,
+        pincode: 522007
       },
       {
+        sr_no: '04',
         name: "Cine Prime",
         city: "Guntur",
         description: "70mm Dolby Atmos",
-        pinCode: 522007,
+        screen:2,
+        pincode: 522007
+      },
+
+      {
+        sr_no: '05',
+        name: "Gowri Shankar",
+        city: "Guntur",
+        description: "Golden Picture",
+        screen:1,
+        pincode: 522007
       },
       {
-        name: "Naaz A/C",
+        sr_no: '06',
+        name: "Naaz Apsara A/C",
         city: "Guntur",
         description: "Dual Screen theatre",
-        pinCode: 522007,
+        screen:2,
+        pincode: 522007
       },
       {
-        name: "Sqami Theatre",
+        sr_no: '07',
+        name: "Naaz Delux A/C",
         city: "Guntur",
-        description: "Dolby Atmos",
-        pinCode: 522007,
+        description: "Dual Screen theatre",
+        screen:1,
+        pincode: 522007
       },
       {
+        sr_no: '08',
+        name: "Swami Theatre",
+        city: "Guntur",
+        screen:1,
+        description: "Dolby Atmos",
+        pincode: 522007
+      },
+      {
+        sr_no: '09',
         name: "Harihar Mahal",
         city: "Guntur",
+        screen:2,
         description: "70mm Dolby Atmos",
-        pinCode: 522007,
+        pincode: 522007
       },
+      {
+        sr_no:'10',
+        name:'Hollywood, Bollywood',
+        city:'Guntur',
+        screen:2,
+        description:'Talkies',
+        pincode:522007
+      }
     ];
     setTheatersData(datafromAPI);
     counterInfo.theater = datafromAPI.length;
@@ -74,26 +113,28 @@ const Admin = () => {
 
   const fetchMoviesData = () => {
     const datafromAPI = [
-      "Kanthara",
-      "Love Today",
-      "PS-1",
-      "RRR",
-      "Prince",
-      "Ginna",
-      "Yashodha",
-      "HIT Case 2",
-      "DJ Tillu",
-      "God father",
-      "Wakanda forever",
-      "Avatar",
-      "Thor Love and Thunder",
-      "Sardhar",
-      "Vikram Hitlist",
-      "Sitha Ramam",
-      "K.G.F Chapeter-2",
-      "Ramsethu",
-      "Brahmmastra",
-      "Dhamki",
+      {srno:'01',
+       movie:'Kanthara',
+       cast:'Rishab Shetty, Sapthami Gowda, Kishore',
+       languages:'Kannada,Telugu,Tamil,Hindhi,Malayalam'},
+       {
+        srno:'02',
+        movie:'Love Today',
+        cast:'Pradeep Ranganatham,Lvana,Satyaraj',
+        languages:'Tamil,Telugu,Kannada,Hindhi,Malayalam'
+       },
+       {
+        srno:'03',
+        movie:'PS-1',
+        cast:'Vikram,Karthi,Aishwarya Rai Bachan,Trisha',
+        languages:'Tamil,Telugu,Kannada,Hindhi,Malayalam'
+       },
+       {
+        srno:'04',
+        movie:'RRR',
+        cast:'Ram charan,JR.NTR, Rajamouli, Alibatt',
+        languages:'Telugu,Tamil,Kannada,Hindhi,Malayalam'
+       }
     ];
     setMoviesData(datafromAPI);
     console.log(setMoviesData);
@@ -199,49 +240,68 @@ const Admin = () => {
         {showMoviesTable && <h1>Movies </h1>}
         {showUsersTable && <h1>Users </h1>}
       </div>
-      <div>
-        {showUsersTable &&
-          usersData.map((user) => <li className="m-2">{user}</li>)}
-        {showMoviesTable &&
-          moviesData.map((movie) => (
-            <>
-              <h4 className="m-2 display-3">{movie}</h4>
-            </>
+      {/* For theater table  */}
+      <div className="container">
+        {showTheaterTable &&
+      <TableContainer component={Paper}>
+         <Table sx={{ minWidth: 650 }} aria-label="simple table">
+         <TableHead>
+          <TableRow>
+            <TableCell align="center">SR.NO</TableCell>
+            <TableCell align="center">Name of Theater</TableCell>
+            <TableCell align="center">Description</TableCell>
+            <TableCell align="center">Location</TableCell>
+            <TableCell align="center">No.of Screens</TableCell>
+          </TableRow>
+          </TableHead>
+          <TableBody>
+          {theatersData.map((theater)=>(
+            <TableRow>
+              <TableCell align="center">{theater.sr_no}</TableCell>
+              <TableCell align="center">{theater.name}</TableCell>
+              <TableCell align="center">{theater.description}</TableCell>
+              <TableCell align="center">{theater.city}</TableCell>
+              <TableCell align="center">{theater.screen}</TableCell>
+            </TableRow>
           ))}
-      </div>
-      <div>
+          </TableBody>
+         </Table>
+      </TableContainer>
+      }
+        </div> 
         <div className="container">
-          <div>
-            <TableContainer component={Paper}>
-              <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Sr.no</TableCell>
-                    <TableCell align="left">Name of the Theater</TableCell>
-                    <TableCell align="left">Description</TableCell>
-                    <TableCell align="left">Location</TableCell>
-                    <TableCell align="left">Screens</TableCell>
-                  </TableRow>
-                </TableHead>
-              {showTheaterTable &&
-              theatersData.map((theater)=>(
-                <>
-                 <TableBody>
-                <TableCell align="left">01</TableCell>
-                <TableCell align="left">{theater.name}</TableCell>
-                <TableCell align="left">{theater.description}</TableCell>
-                <TableCell align="left">{theater.city}</TableCell>
-                <TableCell align="left">{theater.pincode}</TableCell>
-              </TableBody>
-                </>
-              ))
-              }
-              </Table>
-            </TableContainer>
-          </div>
-        </div>
+         {showMoviesTable && 
+         <TableContainer component={Paper}>
+           <Table sx={{ minWidth: 650 }} aria-label="simple table">
+         <TableHead>
+          <TableRow>
+            <TableCell align="center">SR.NO</TableCell>
+            <TableCell align="center">Name of Movie</TableCell>
+            <TableCell align="center">Cast</TableCell>
+            <TableCell align="center">Language</TableCell>
+          </TableRow>
+          </TableHead>
+          <TableBody>
+            {moviesData.map((movie)=>(
+             <TableRow>
+             <TableCell align="center">{movie.srno}</TableCell>
+             <TableCell align="center">{movie.movie}</TableCell>
+             <TableCell align="center">{movie.cast}</TableCell>
+             <TableCell align="center">{movie.languages}</TableCell>
+             </TableRow>
+            ))
+
+            }
+          </TableBody>
+          </Table>
+         </TableContainer>
+         
+         
+         
+         
+         }
+          </div>    
       </div>
-    </div>
   );
 };
 
